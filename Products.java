0f5f0c4a27e14e5592b30a8416b004dab58874c0
AkashYadwad <akashyadwad790@gmail.com>
2025-06-17 14:50:56 +0530

    java

---
+++ Products.java
@@ -0,0 +1,16 @@
+class Products{
+public static void main(String prods[]) { 
+	System.out.println("List of the products and its description");
+	String oneplusnord= "The OnePlus Nord 2T 128GB Green 5G is a powerful mid-range smartphone that's very fast with average use";
+     System.out.println(oneplusnord);
+	 String MotorolaEdge="Get 5000 mAh battery in this ultra thin design. Power your day in just 15 minutes with 68 W TurboPower fast charging support. Which means 30 hours of battery life for all the crazy things you want to explore";
+	 System.out.println(MotorolaEdge);
+	 String SamsungGalaxy="The Samsung Galaxy M05 offers 4GB of RAM and 64GB of memory, allowing you to store all your important files and run multiple apps smoothly.";
+	 System.out.println(SamsungGalaxy);
+	 String Realme="Introducing the realme 14x 5G, a powerful smartphone that combines sleek design with cutting-edge technology. ";
+	 System.out.println(Realme);
+	 String Redmi="Redmi 14C 5G – Unleash Next-Generation PerformanceExperience the perfect combination of power, performance, and style with the Redmi 14C 5G";
+	 System.out.println(Redmi);
+	 
+}
+}
---
+++ Sports.java
@@ -0,0 +1,8 @@
+class Sports{
+public static void main(String args[]){
+	System.out.println("hello Rohit sharmas");
+	System.out.println("highest Score is 264");
+	System.out.println(67 + 89 + "baba");
+}
+
+}
