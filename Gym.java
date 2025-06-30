0761cb25633f6ba20418cade633af4bfe48073fb
AkashYadwad <akashyadwad790@gmail.com>
2025-06-30 12:29:18 +0530

    java

---
+++ Gym.java
@@ -0,0 +1,29 @@
+class Gym
+{
+  
+	 static int id=23112;
+	 static int phno=18771980909;
+	 static String email="Gym@gmail.com";
+	 static String openingTime="6am";
+	 static String closingTime="10pm";
+	 static String gymOwner="Anup";
+	 static String location="Kempapura";
+	 static int products=23;
+	 static int dumbels=65;
+	 static boolean availableParking=true;
+	 static boolean weightSection=true; 
+	 static String establishedYear="2016";
+	  
+	 
+	 public static void main(String args[]){
+		 System.out.println("the ID is"+id);
+		 System.out.println("the phno is:"+phno);
+         System.out.println("the email is:"+email);
+         System.out.println("the opening time"+openingTime);
+         System.out.println("the closingtime"+closingTime);
+         System.out.println("the gym owner"+gymOwner);		
+         System.out.println("the opening year"+establishedYear);
+		 
+	 
+  }
+}
