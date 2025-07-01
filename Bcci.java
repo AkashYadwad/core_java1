af8e3b401ed9c96e2f0e214286e5893e291f68e9
AkashYadwad <akashyadwad790@gmail.com>
2025-07-01 11:40:16 +0530

    java

---
+++ Bcci.java
@@ -0,0 +1,36 @@
+class Bcci
+{
+	 static String player1="Rohit sharma";
+	 static String player2="virat Kholi";
+	 static String player3="jaddu";
+	 static String player4="Bumrah";
+	 static String player5="Ashwin";
+	 static String player6="Bhuvi";
+	 static String player7="Siraj";
+	 static String player8="Hardik";
+	 static String player9="surya";
+	 static String player10="Kuldeep";
+	 static String player11="Axar";
+	 static String Players[]={player1,player2,player3,player4,player5,player6,player7,player8,player9,player10,player11,"sixer siddu","kapildev"};
+	 public static void main(String cric[]){
+		 int n=2;
+		 
+		 for(int i=0; i<n; i++)
+		 {
+		 System.out.println("the top Batsmen is:");
+		 System.out.println(Players[0]);
+		 System.out.println(Players[9]);
+		 System.out.println(Players[8]);
+		 System.out.println(Players[1]);
+		 System.out.println("The ToP Bowlers is:");
+		 System.out.println(Players[6]);
+		 System.out.println(Players[7]);
+		 System.out.println(Players[4]);
+		 System.out.println("the Top Allrounder is:");
+		 System.out.println(Players[11]);
+         System.out.println(Players[10]);
+		 System.out.println(Players[5]);
+		 
+		 } 
+	 }
+}
