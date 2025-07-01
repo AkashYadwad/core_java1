6097bddf547bd308371b21bab14c9c821b5c4930
AkashYadwad <akashyadwad790@gmail.com>
2025-07-01 10:08:43 +0530

    javafile

---
+++ Medicine.java
@@ -0,0 +1,16 @@
+class Medicine
+{
+	  static String MedicineName="chorex76";
+	  static String MedicineID="334jjnjr";
+	  static int noOfDoctors=34;
+		static String address="Belagavi";
+		static String type="Dengue";
+	public static void main(String medi[]){
+		
+		System.out.println("the name of the medicine is:"+MedicineName);
+		System.out.println("the medicine id is:"+MedicineID); 
+		System.out.println("the number of doctor is:"+noOfDoctors);
+		System.out.println("the addredsss is"+address);
+		
+	}
+}  
