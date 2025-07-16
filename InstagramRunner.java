06a6533182444c5c29f7f66916f2951386082480
AkashYadwad <akashyadwad790@gmail.com>
2025-07-16 10:47:28 +0530

    overload

---
+++ InstagramRunner.java
@@ -0,0 +1,9 @@
+
+class InstagramRunner{
+
+public static void main(String insta[]){
+
+boolean loginUserWithGmail = Instagram.logIn("Aku@gmail.com","123abc");
+boolean loginUserWithNum = Instagram.logIn(9036634798L,"145541");
+}
+}
